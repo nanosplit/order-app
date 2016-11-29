710a3bd457c00be6841cf19314b0811df2f7b4c1
Jacob <jacob@mpulsedesign.com>
2016-11-28 23:42:09 -0600

    notes

--- app/magic/magic.rb
+++ app/magic/magic.rb
@@ -5,6 +5,18 @@
 
 class Magic
 
+  # save_cart you would do as save_cart(params)
+  # params just needs to be a hash of whatever
+  # values you need to populate the cart. In
+  # this example, you would pass the venue object
+  # and the products object. This information
+  # could be obtained from one of the backbars
+  # views, they select the qty for a product
+  # they previously added to their backbar, and
+  # then when they submit the form, it will
+  # pass those params to save_cart and let
+  # it do it's thing.... 
+
   def save_cart
     venue = Venue.first
     products = Product.all
@@ -28,15 +40,28 @@ class Magic
     Cart.create(venue_id: venue.id, products: hash)
   end
 
+
+  # you would want to do something like create_order(cart)
+  # and do your query outside this object. So maybe in your
+  # controller, you'd have:
+  # cart = Venue.carts.first
+  # Magic.new.create_order(cart)
+
   def create_order
-    cart = Cart.last
+    cart = Cart.last # you would remove this since you're passing cart on the call
     products = cart.products
     price = []
 
+    # iterate over each product and get the price to sum for total
     products.each do |item|
       price << item[1]['price'] * item[1]['quantity']
     end
     total = price.inject(0, &:+)
+
+    # before you do the final Order.create, you would at this point want to
+    # run whatever 3rd party transactios you have, such as posting a
+    # transaction to the OrderWise system. Then, pending the success,
+    # you can then create the order record.
 
     Order.create(venue_id: cart.venue_id, total: total, products: cart.products)
   end
