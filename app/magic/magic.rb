fd9ecac2ece7e87a5eb66b474210e09d2559aa47
Jacob <jacob@mpulsedesign.com>
2016-11-28 22:56:06 -0600

    add_up_cart function

--- app/magic/magic.rb
+++ app/magic/magic.rb
@@ -1,6 +1,7 @@
 
 
-# Use: MagicStuff.new.save_cart
+# Use: Magic.new.save_cart
+#      Magic.new.add_up_cart
 
 class Magic
 
@@ -25,6 +26,18 @@ class Magic
     end
 
     Cart.create(venue_id: venue.id, products: hash)
+  end
+
+  def add_up_cart
+    cart = Cart.last
+    products = cart.products
+    price = []
+
+    products.each do |item|
+      price << item[1]['price'] * item[1]['quantity']
+    end
+    total = price.inject(0, &:+)
+    return (total / 100).to_f
   end
 
 end
